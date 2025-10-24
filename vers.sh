6d14d8e64be1e1ca0dae5aac219708fe93b6dbcf
Sid Roberts <sid@sidroberts.co.uk>
2025-10-24 15:23:53 +0900

    Tidied up shell script.

--- vers.sh
+++ vers.sh
@@ -1,10 +1,12 @@
 #!/bin/sh
 
-if git describe >/dev/null 2>&1
-then
+set -eu
+
+if git describe >/dev/null 2>&1; then
     git describe --tags --match=dev* | sed s/^dev// | tr - + | tr -d '\n'
-    if ! git diff --quiet HEAD
-    then printf +mod
+
+    if ! git diff --quiet HEAD; then
+        printf +mod
     fi
 else
     printf unknown
